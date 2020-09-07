1047e263cbc209a7d803627b5c8c0c099ddf23d1
vorillaz <theodorevorillas@gmail.com>
2020-09-07 23:08:17 +0300

    ci: yo

--- bash.sh
+++ bash.sh
@@ -1,10 +1,10 @@
 LATEST_COMMIT=$(git log --oneline -n 1 HEAD)
-COMMIT_REGEX="(develop$|staging|release//*)"
+COMMIT_REGEX="(develop|staging|release)"
 
 
 echo $LATEST_COMMIT;
 
-if [[ $LATEST_COMMIT =~ $BRANCH_REGEX ]]; then
+if [[ $LATEST_COMMIT =~ $COMMIT_REGEX ]]; then
     echo "This is a release, kill this one!"
 else
   echo "Prepare a new release"
